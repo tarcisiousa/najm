8443f030b83a1ebc11165d913fcd47a450bd9757
Tarcisio Alcantara <tarcisiousa22@gmail.com>
2024-10-25 11:56:37 -0300

    config_allowed

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5oq6o&8+)9os^-^cslk-8__ux9epv2adb!iwhkp11-0tg@%h#_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
